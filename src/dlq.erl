ce7e710857beb93275f91b6e10fe9d7f9c9b5274
diddyhsv <ditmarlange@hotmail.de>
2018-10-23 11:35:40 +0200

    DLQ Kommentare
    DLQ Kommentare drin

--- src/dlq.erl
+++ src/dlq.erl
@@ -12,20 +12,34 @@
 %% API
 -export([initDLQ/2,delDLQ/1,expectedNr/1,push2DLQ/3,deliverMSG/4,listDLQ/1,lengthDLQ/1]).
 
+%% Initialisiert die DLQ
 initDLQ(Size,Datei) ->
   util:logging(Datei, "dlq>>> initialisiert mit Kapazität " ++ util:to_String(Size) ++ "\n"),
+  %% Die DLQ enthält eine sortierte Liste, 
+  %% in die die empfangenen Nachrichten geschrieben werden.
+  %% Erstellt eine Queue DLQ mit Grösse size.
   [[], Size].
 
+%% delDLQ(Queue) Löscht die DLQ und gibt ok zurück.
 delDLQ(_) ->
   ok.
 
+%% Liefert die NNr. 
+%% der nächsten Nachricht, 
+%% die in die DLQ geschrieben werden darf.
 expectedNr([[],_]) ->
+%% Wenn die DLQ leer ist, 
+%% 1 zurückgeben
   1;
 expectedNr(Queue) ->
   [DLQ, _] = Queue,
+  %% Ansonsten solange durch die Nachrichten iterieren, 
+  %% bis man bei der letzten angekommen ist (last(DLQ)).
   [NNr,_Msg,_TSclientout,_TShbqin,_TSdlqin] = last(DLQ),
+  %% Dessen NNr + 1 zurückgeben.
   NNr + 1.
 
+%% Speichert eine Nachricht in der DLQ und fügt einen Eingangszeitstempel an.
 push2DLQ([NNr,Msg,Tsclientout,Tshbqin],Queue,Datei) ->
   [DLQ,Size] = Queue,
   Len = lengthDLQ(Queue),
@@ -34,34 +48,48 @@ push2DLQ([NNr,Msg,Tsclientout,Tshbqin],Queue,Datei) ->
       util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNr) ++ " in DLQ eingefügt.\n"),
       [append(DLQ,[[NNr,Msg,Tsclientout,Tshbqin,erlang:timestamp()]]),Size];
     true ->
+      %% Wenn die DLQ voll ist, wird die älteste Nachricht aus der DLQ entfernt.
       [[DNNr,_Msg,_Tsclientout,_Tshbqin,_Tsdlqin]|T] = DLQ,
-      util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNr) ++ " in DLQ eingefügt.\n"),
       util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(DNNr) ++ " aus DLQ gelöscht.\n"),
+      util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNr) ++ " in DLQ eingefügt.\n"),
       [append(T,[[NNr,Msg,Tsclientout,Tshbqin,erlang:timestamp()]]),Size]
   end.
 
+%% Sendet eine Nachricht an den Leser.
 deliverMSG(MSGNr,ClientPID,Queue,Datei) ->
   [DLQ, _] = Queue,
+  %% Überprüft ob in der DLQ eine Nachricht mit NNr gleich MSGNr vorhanden ist (keyfind(MSGNr, DLQ)).
   Bool = keyfind(MSGNr,DLQ),
   Expect = expectedNr(Queue),
   TSdlqout = erlang:timestamp(),
 
   case Bool of
     false ->
+      %% Falls diese Nachricht nicht vorhanden ist, wird die erste Nachricht der DLQ genommen.
       NNNr = findNext(MSGNr,Expect,DLQ),
       %util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNNr) ++ " ist die nächste in der DLQ vorhandene Nachricht nach " ++ util:to_String(MSGNr) ++ ".\n"),
       [NNr,Msg,TSclientout,TShbqin,TSdlqin] = keyfind(NNNr,DLQ),
       [LNNr,_,_,_,_] = last(DLQ),
+      %% Überprüft ob weitere Nachrichten in der DLQ vorhanden sind, 
+      %% indem die nächste von der DLQ erwartete NNr (Expect) mit der NNr der Nachricht (MSGNr) verglichen wird.
       if
         LNNr < MSGNr ->
           ClientPID ! {reply, [-1, nokA, 0,0,0,0], true},
           util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNr) ++ " an Client " ++ util:to_String(ClientPID) ++ " ausgeliefert.\n"),
           -1;
         (Expect - 1) > MSGNr ->
+          %% Fügt der Nachricht einen Zeitstempel TSdlqout an.
+          %% Ansonsten ((Expect - 1) > MSGNr) ist ‘Terminated’ ‘false’.
+          %% Sendet die Nachricht an den Client.
           ClientPID ! {reply,[NNr,Msg ++ util:to_String(vsutil:now2string(TSdlqout)),TSclientout,TShbqin,TSdlqin,TSdlqout], false},
           util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNr) ++ " an Client " ++ util:to_String(ClientPID) ++ " ausgeliefert.\n"),
+          %% Gibt die gesendete NNr zurück.
           NNr;
         (Expect - 1) == MSGNr ->
+          %% Ist die erwartete NNr (Expect) um 1 größer als die dieser Nachricht (MSGNr), 
+          %% so wird der Parameter ‘Terminated’ auf ‘true’ gesetzt, 
+          %% als Zeichen dafür, 
+          %% dass dies die letzte Nachricht ist.
           ClientPID ! {reply, [NNr,Msg ++ util:to_String(vsutil:now2string(TSdlqout)),TSclientout,TShbqin,TSdlqin,erlang:timestamp()], true},
           util:logging(Datei, "dlq>>> Nachricht " ++ util:to_String(NNr) ++ " an Client " ++ util:to_String(ClientPID) ++ " ausgeliefert.\n"),
           NNr
@@ -81,10 +109,14 @@ deliverMSG(MSGNr,ClientPID,Queue,Datei) ->
   end
 .
 
+%% Erzeugt eine Liste der in der DLQ vorhandenen Nachrichtnummern und gibt diese zurück.
 listDLQ(Queue) ->
   [DLQ,_] = Queue,
   getNNrs(DLQ).
 
+%% Zählt die Anzahl der Nachrichten, 
+%% die in der DLQ sind, 
+%% und gibt diese zurück.
 lengthDLQ(Queue) ->
   [DLQ,_] = Queue,
   listLength(DLQ).
